56f585099fb7c9c307b5c744c6f194a4d0542034
coastalwebdeveloper <coastal.webdeveloper@gmail.com>
2026-09-03 14:01:44 +0530

    Update map location to exact Shri Ganesh Silks pin

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -12,8 +12,8 @@ export const SITE = {
   rating: "4.7",
   reviewCount: 405,
   mapEmbed:
-    "https://www.google.com/maps?q=Thekkatte,+Kundapura,+Karnataka&output=embed",
-  mapLink: "https://www.google.com/maps/search/?api=1&query=Thekkatte+Kundapura+Karnataka",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.0!2d74.6993685!3d13.5495835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbc914251476b47%3A0x6dc7f54b7b84fa77!2sShri%20Ganesh%20silks!5e0!3m2!1sen!2sin!4v1",
+  mapLink: "https://www.google.com/maps/place/Shri+Ganesh+silks/@13.5495835,74.6993685,17z/data=!3m1!4b1!4m6!3m5!1s0x3bbc914251476b47:0x6dc7f54b7b84fa77!8m2!3d13.5495783!4d74.7019434!16s%2Fg%2F11cn5fjzmx",
   instagram: "https://www.instagram.com/ganeshsilks_?igsh=MW5tNmNwZGNqbHIxNQ==",
   facebook: "https://facebook.com",
 };
